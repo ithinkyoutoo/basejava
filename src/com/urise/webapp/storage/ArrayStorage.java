96cdb921d3cc3b025929497c261d7d34f27cac77
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-08-21 16:26:06 +0800

    update ArrayStorage: implement Storage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,7 +4,7 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class ArrayStorage {
+public class ArrayStorage implements Storage {
 
     private static final int CAPACITY = 10_000;
 
