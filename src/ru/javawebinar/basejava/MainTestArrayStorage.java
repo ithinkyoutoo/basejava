b38dbcbedc9110c39abfad75cf25cee57860b4ba
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-08-27 23:00:56 +0800

    update MainTestArrayStorage
    - поменял тип ARRAY_STORAGE.
    - сделал private ARRAY_STORAGE и printAll().

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -2,9 +2,10 @@ package ru.javawebinar.basejava;
 
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.ArrayStorage;
+import ru.javawebinar.basejava.storage.Storage;
 
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -33,7 +34,7 @@ public class MainTestArrayStorage {
         System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
-    static void printAll() {
+    private static void printAll() {
         System.out.println("\nGet All");
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
