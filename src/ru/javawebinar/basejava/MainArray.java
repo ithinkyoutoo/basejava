bf81dfa3fada272cb553dbe9b1eb26875f042571
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-09-10 16:02:33 +0800

    update Resume
    - do final uuid.
    - create constructors, delete setUuid():
      - update MainArray, MainTestArrayStorage, SortedArrayStorage.

--- src/ru/javawebinar/basejava/MainArray.java
+++ src/ru/javawebinar/basejava/MainArray.java
@@ -38,8 +38,7 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
@@ -51,8 +50,7 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.get(uuid));
                     break;
                 case "update":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;
