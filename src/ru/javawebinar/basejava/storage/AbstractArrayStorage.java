6cfdf90be048caeb365b342a34068be11113d7e2
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-08 18:26:47 +0800

    fix & update AbstractArrayStorage
    - move delete() to AbstractStorage.
    - implement abstract delete().
    - rename setResume(), saveResume() & getResume() to update(), save() & get().
    - update save() & abstracts findIndex(), saveResume().
    - implement isExist().
    - delete hasNotCapacity().

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -1,6 +1,6 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
@@ -17,17 +17,6 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         countResume = 0;
     }
 
-    public final void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            countResume--;
-            deleteResume(index);
-            storage[countResume] = null;
-        }
-    }
-
     public final Resume[] getAll() {
         return Arrays.copyOf(storage, countResume);
     }
@@ -36,21 +25,36 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         return countResume;
     }
 
-    protected abstract int findIndex(String uuid);
-
-    protected void setResume(Resume resume, int index) {
-        storage[index] = resume;
+    protected void update(Resume resume, Object searchKey) {
+        storage[(int) searchKey] = resume;
     }
 
-    protected boolean hasNotCapacity() {
-        return countResume == CAPACITY;
+    protected void save(Resume resume, Object searchKey) {
+        if (countResume == CAPACITY) {
+            throw new StorageException("Хранилище заполнено, вы не можете добавить новое резюме", resume.getUuid());
+        } else {
+            saveResume(resume, (int) searchKey);
+            countResume++;
+        }
     }
 
-    protected abstract void saveResume(Resume resume, int index);
-
-    protected Resume getResume(int index) {
-        return storage[index];
+    protected Resume get(String uuid, Object searchKey) {
+        return storage[(int) searchKey];
     }
+
+    protected void delete(String uuid, Object searchKey) {
+        countResume--;
+        deleteResume((int) searchKey);
+        storage[countResume] = null;
+    }
+
+    protected boolean isExist(Object searchKey) {
+        return (int) searchKey < 0;
+    }
+
+    protected abstract Object findIndex(String uuid);
 
     protected abstract void deleteResume(int index);
+
+    protected abstract void saveResume(Resume resume, int index);
 }
