8a16101c4c822653e5b42079734b474f12f3e23b
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-31 19:11:22 +0800

    update MainArray
    - update printAll().

--- src/ru/javawebinar/basejava/MainArray.java
+++ src/ru/javawebinar/basejava/MainArray.java
@@ -7,6 +7,7 @@ import ru.javawebinar.basejava.storage.Storage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Interactive test for com.urise.webapp.storage.ArrayStorage implementation
@@ -68,9 +69,9 @@ public class MainArray {
     }
 
     private static void printAll() {
-        Resume[] all = ARRAY_STORAGE.getAll();
+        List<Resume> all = ARRAY_STORAGE.getAllSorted();
         System.out.println("----------------------------");
-        if (all.length == 0) {
+        if (all.isEmpty()) {
             System.out.println("Empty");
         } else {
             for (Resume r : all) {
