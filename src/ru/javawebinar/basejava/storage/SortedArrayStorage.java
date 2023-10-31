a074b4e50b9267dc74ee513d004e87a7af582b01
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-31 19:08:16 +0800

    update SortedArrayStorage
    - create RESUME_COMPARATOR.
    - update findSearchKey().

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -3,13 +3,16 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected Object findSearchKey(String uuid) {
         Resume resume = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, countResume, resume);
+        return Arrays.binarySearch(storage, 0, countResume, resume, RESUME_COMPARATOR);
     }
 
     @Override
