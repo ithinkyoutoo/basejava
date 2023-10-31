cdae09188bb839aa6ae9c9dd6744c67405bf27dc
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-31 19:16:28 +0800

    create AllStorageTest

---
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ListStorageTest.class,
+        MapStorageTest.class,
+        MapHashStorageTest.class,
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class
+})
+public class AllStorageTest {
+}
