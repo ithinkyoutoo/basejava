c65028c9caf48d9828fa95c1097eb41b20b49188
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-31 19:06:23 +0800

    update AbstractArrayStorage
    - implement doGetAll().
    - delete getAll().

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -4,6 +4,7 @@ import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.List;
 
 public abstract class AbstractArrayStorage extends AbstractStorage {
 
@@ -17,18 +18,16 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         countResume = 0;
     }
 
-    public final Resume[] getAll() {
-        return Arrays.copyOf(storage, countResume);
-    }
-
     public final int size() {
         return countResume;
     }
 
+    @Override
     protected void doUpdate(Resume resume, Object searchKey) {
         storage[(int) searchKey] = resume;
     }
 
+    @Override
     protected void doSave(Resume resume, Object searchKey) {
         if (countResume == CAPACITY) {
             throw new StorageException("Хранилище заполнено, вы не можете добавить новое резюме", resume.getUuid());
@@ -38,20 +37,29 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         }
     }
 
+    @Override
     protected Resume doGet(Object searchKey) {
         return storage[(int) searchKey];
     }
 
+    @Override
     protected void doDelete(Object searchKey) {
         countResume--;
         deleteResume((int) searchKey);
         storage[countResume] = null;
     }
 
+    @Override
+    protected final List<Resume> doGetAll() {
+        return Arrays.asList(Arrays.copyOf(storage, countResume));
+    }
+
+    @Override
     protected boolean isExist(Object searchKey) {
         return (int) searchKey >= 0;
     }
 
+    @Override
     protected abstract Object findSearchKey(String uuid);
 
     protected abstract void deleteResume(int searchKey);
