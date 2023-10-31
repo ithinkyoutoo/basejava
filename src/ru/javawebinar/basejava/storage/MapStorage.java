c3ee3dc90b616d94c954d44f4cb437f2aef140f8
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-10-31 19:05:19 +0800

    update MapStorage
    - implement doGetAll().
    - delete getAll().

--- src/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/ru/javawebinar/basejava/storage/MapStorage.java
@@ -2,7 +2,9 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
@@ -13,34 +15,41 @@ public class MapStorage extends AbstractStorage {
         storage.clear();
     }
 
-    public Resume[] getAll() {
-        return storage.values().toArray(new Resume [0]);
-    }
-
     public int size() {
         return storage.size();
     }
 
+    @Override
     protected void doUpdate(Resume resume, Object searchKey) {
         storage.put((String) searchKey, resume);
     }
 
+    @Override
     protected void doSave(Resume resume, Object searchKey) {
         storage.put((String) searchKey, resume);
     }
 
+    @Override
     protected Resume doGet(Object searchKey) {
         return storage.get((String) searchKey);
     }
 
+    @Override
     protected void doDelete(Object searchKey) {
         storage.remove((String) searchKey);
     }
 
+    @Override
+    protected List<Resume> doGetAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
     protected Object findSearchKey(String uuid) {
         return uuid;
     }
 
+    @Override
     protected boolean isExist(Object searchKey) {
         return storage.containsKey((String) searchKey);
     }
