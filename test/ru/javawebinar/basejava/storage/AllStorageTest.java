b0e8912698eb76bf6d30b267ed8a1b9d03f7c65c
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-11-11 17:57:52 +0800

    fix AllStorageTest

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ListStorageTest.class,
+@Suite.SuiteClasses({
+        ListStorageTest.class,
         MapStorageTest.class,
         MapResumeStorageTest.class,
         ArrayStorageTest.class,
