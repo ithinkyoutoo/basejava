eca65668872c46bd0adccf7e809016b72cee3947
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-11-28 13:57:21 +0800

    update AbstractStorage: introduce Logging

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,29 +6,37 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     private static final Comparator<Resume> RESUME_COMPARATOR
             = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
 
     public final void update(Resume resume) {
+        LOG.info("Update " + resume);
         doUpdate(resume, getExistingSearchKey(resume.getUuid()));
     }
 
     public final void save(Resume resume) {
+        LOG.info("Save " + resume);
         doSave(resume, getNotExistingSearchKey(resume.getUuid()));
     }
 
     public final Resume get(String uuid) {
+        LOG.info("Get Resume " + uuid);
         return doGet(getExistingSearchKey(uuid));
     }
 
     public final void delete(String uuid) {
+        LOG.info("Delete Resume " + uuid);
         doDelete(getExistingSearchKey(uuid));
     }
 
     public final List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> resume = doGetAll();
         resume.sort(RESUME_COMPARATOR);
         return resume;
@@ -53,12 +61,14 @@ public abstract class AbstractStorage<SK> implements Storage {
         if (isExist(searchKey)) {
             return searchKey;
         }
+        LOG.warning("Resume " + uuid + " not exist");
         throw new NotExistStorageException(uuid);
     }
 
     private SK getNotExistingSearchKey(String uuid) {
         SK searchKey = findSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
