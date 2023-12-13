f9c321282965c42028d869bcb92ddd387bd91911
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-12-13 21:10:53 +0800

    update StorageException
    - add StorageException(file, message).

--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -1,8 +1,15 @@
 package ru.javawebinar.basejava.exception;
 
+import java.io.File;
+
 public class StorageException extends RuntimeException {
 
     private final String uuid;
+
+    public StorageException(File file, String message) {
+        super(file.getName() + message);
+        this.uuid = null;
+    }
 
     public StorageException(String message, String uuid) {
         super(message);
