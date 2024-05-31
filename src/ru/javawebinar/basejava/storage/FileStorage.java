403b36593e17248bd28fa637f6d08d397c462f6b
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-05-31 17:23:57 +0800

    move FileStorage & PathStorage from storage.serialization to storage

--- src/ru/javawebinar/basejava/storage/FileStorage.java
+++ src/ru/javawebinar/basejava/storage/FileStorage.java
@@ -1,8 +1,8 @@
-package ru.javawebinar.basejava.storage.serialization;
+package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
-import ru.javawebinar.basejava.storage.AbstractStorage;
+import ru.javawebinar.basejava.storage.serialization.Serialization;
 
 import java.io.*;
 import java.util.ArrayList;
