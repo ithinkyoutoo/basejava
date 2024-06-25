d0eb94bb53b8624a8b2b20843e7514f5b7c7431a
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-06-25 15:49:22 +0800

    delete BiCustomConsumer

--- src/ru/javawebinar/basejava/util/BiCustomConsumer.java
+++
@@ -1,9 +0,0 @@
-package ru.javawebinar.basejava.util;
-
-import java.io.IOException;
-
-@FunctionalInterface
-public interface BiCustomConsumer<K, V> {
-
-    void accept(K k, V v) throws IOException;
-}
