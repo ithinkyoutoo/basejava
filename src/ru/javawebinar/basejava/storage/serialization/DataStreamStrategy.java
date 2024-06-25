6a54a0ae30e2d41e59fd6cdd6882363389507d7c
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-06-25 15:48:22 +0800

    fix DataStreamStrategy
    - refactor write(map, ...), write(list, ...), write(section, ...) to
               write(collection).
    - fix read(): change CustomConsumer to CustomRunnable.
    - fix readList(): change CustomConsumer to CustomSupplier.
    - add writeLocalDate() & readLocalDate().
    - rename & update:
      - getItems() to readItems().
      - readContacts() & readSections() to addContacts() & addSections().
      - getCompanies() & getPeriods() to readCompanies() & readPeriods().
    - update:
      - writeCompanies() & writePeriods() to writeCompanies().
      - doWrite(), doRead(), writeContacts() & writeSections().

--- src/ru/javawebinar/basejava/storage/serialization/DataStreamStrategy.java
+++ src/ru/javawebinar/basejava/storage/serialization/DataStreamStrategy.java
@@ -1,24 +1,25 @@
 package ru.javawebinar.basejava.storage.serialization;
 
 import ru.javawebinar.basejava.model.*;
-import ru.javawebinar.basejava.util.BiCustomConsumer;
 import ru.javawebinar.basejava.util.CustomConsumer;
+import ru.javawebinar.basejava.util.CustomRunnable;
+import ru.javawebinar.basejava.util.CustomSupplier;
 
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public class DataStreamStrategy implements Serialization {
 
     @Override
-    public void doWrite(Resume r, OutputStream os) throws IOException {
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
-            dos.writeUTF(r.getUuid());
-            dos.writeUTF(r.getFullName());
-            writeContacts(r.getContacts(), dos);
-            writeSections(r.getSections(), dos);
+            dos.writeUTF(resume.getUuid());
+            dos.writeUTF(resume.getFullName());
+            writeContacts(resume, dos);
+            writeSections(resume, dos);
         }
     }
 
@@ -28,114 +29,95 @@ public class DataStreamStrategy implements Serialization {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            readContacts(resume.getContacts(), dis);
-            readSections(resume.getSections(), dis);
+            addContacts(resume, dis);
+            addSections(resume, dis);
             return resume;
         }
     }
 
-    private void writeContacts(Map<ContactType, String> contacts, DataOutputStream dos) throws IOException {
-        write(contacts, dos, (type, text) -> {
-            dos.writeUTF(type.name());
-            dos.writeUTF(text);
+    private void writeContacts(Resume resume, DataOutputStream dos) throws IOException {
+        write(resume.getContacts().entrySet(), dos, contact -> {
+            dos.writeUTF(contact.getKey().name());
+            dos.writeUTF(contact.getValue());
         });
     }
 
-    private void writeSections(Map<SectionType, Section> sections, DataOutputStream dos) throws IOException {
-        write(sections, dos, (type, section) -> {
+    private void writeSections(Resume resume, DataOutputStream dos) throws IOException {
+        write(resume.getSections().entrySet(), dos, entry -> {
+            SectionType type = entry.getKey();
+            Section section = entry.getValue();
             dos.writeUTF(type.name());
             switch (type) {
-                case OBJECTIVE, PERSONAL -> write((TextSection) section, s -> dos.writeUTF(s.getText()));
-                case ACHIEVEMENT, QUALIFICATIONS ->
-                        write((ListSection) section, s -> write(s.getItems(), dos, dos::writeUTF));
-                case EXPERIENCE, EDUCATION ->
-                        write((CompanySection) section, s -> writeCompanies(s.getCompanies(), dos));
+                case OBJECTIVE, PERSONAL -> dos.writeUTF(((TextSection) section).getText());
+                case ACHIEVEMENT, QUALIFICATIONS -> write(((ListSection) section).getItems(), dos, dos::writeUTF);
+                case EXPERIENCE, EDUCATION -> writeCompanies(((CompanySection) section).getCompanies(), dos);
             }
         });
     }
 
     private void writeCompanies(List<Company> companies, DataOutputStream dos) throws IOException {
-        write(companies, dos, c -> {
-            dos.writeUTF(c.getName());
-            String website = c.getWebsite();
+        write(companies, dos, company -> {
+            dos.writeUTF(company.getName());
+            String website = company.getWebsite();
             if (isNonNull(website, dos)) {
                 dos.writeUTF(website);
             }
-            writePeriods(c.getPeriods(), dos);
+            write(company.getPeriods(), dos, period -> {
+                writeLocalDate(period.getBeginDate(), dos);
+                writeLocalDate(period.getEndDate(), dos);
+                dos.writeUTF(period.getTitle());
+                List<String> description = period.getDescription();
+                if (isNonNull(description, dos)) {
+                    write(description, dos, dos::writeUTF);
+                }
+            });
         });
     }
 
-    private void writePeriods(List<Company.Period> periods, DataOutputStream dos) throws IOException {
-        write(periods, dos, p -> {
-            dos.writeUTF(p.getBeginDate().toString());
-            dos.writeUTF(p.getEndDate().toString());
-            dos.writeUTF(p.getTitle());
-            List<String> description = p.getDescription();
-            if (isNonNull(description, dos)) {
-                write(description, dos, (dos::writeUTF));
-            }
-        });
+    private void addContacts(Resume resume, DataInputStream dis) throws IOException {
+        read(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
     }
 
-    private void readContacts(Map<ContactType, String> contacts, DataInputStream dis) throws IOException {
-        read(contacts, dis, c -> c.put(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
-    }
-
-    private void readSections(Map<SectionType, Section> sections, DataInputStream dis) throws IOException {
-        read(sections, dis, s -> {
+    private void addSections(Resume resume, DataInputStream dis) throws IOException {
+        read(dis, () -> {
             SectionType type = SectionType.valueOf(dis.readUTF());
-            switch (type) {
-                case OBJECTIVE, PERSONAL -> s.put(type, new TextSection(dis.readUTF()));
-                case ACHIEVEMENT, QUALIFICATIONS -> s.put(type, new ListSection(getItems(dis)));
-                case EXPERIENCE, EDUCATION -> s.put(type, new CompanySection(getCompanies(dis)));
-            }
+            resume.setSection(type, switch (type) {
+                        case OBJECTIVE, PERSONAL -> new TextSection(dis.readUTF());
+                        case ACHIEVEMENT, QUALIFICATIONS -> new ListSection(readItems(dis));
+                        case EXPERIENCE, EDUCATION -> new CompanySection(readCompanies(dis));
+                    }
+            );
         });
     }
 
-    private List<Company> getCompanies(DataInputStream dis) throws IOException {
-        return readList(dis, c -> {
+    private List<Company> readCompanies(DataInputStream dis) throws IOException {
+        return readList(dis, () -> {
             String name = dis.readUTF();
             if (dis.readBoolean()) {
                 String website = dis.readUTF();
-                c.add(new Company(name, website, getPeriods(dis)));
-            } else {
-                c.add(new Company(name, getPeriods(dis)));
+                return new Company(name, website, readPeriods(dis));
             }
+            return new Company(name, readPeriods(dis));
         });
     }
 
-    private List<Company.Period> getPeriods(DataInputStream dis) throws IOException {
-        return readList(dis, p -> {
-            LocalDate beginDate = LocalDate.parse(dis.readUTF());
-            LocalDate endDate = LocalDate.parse(dis.readUTF());
+    private List<Company.Period> readPeriods(DataInputStream dis) throws IOException {
+        return readList(dis, () -> {
+            LocalDate beginDate = readLocalDate(dis);
+            LocalDate endDate = readLocalDate(dis);
             String title = dis.readUTF();
             if (dis.readBoolean()) {
-                List<String> description = getItems(dis);
-                p.add(new Company.Period(beginDate, endDate, title, description));
-            } else {
-                p.add(new Company.Period(beginDate, endDate, title));
+                List<String> description = readItems(dis);
+                return new Company.Period(beginDate, endDate, title, description);
             }
+            return new Company.Period(beginDate, endDate, title);
         });
     }
 
-    private List<String> getItems(DataInputStream dis) throws IOException {
-        return readList(dis, s -> s.add(dis.readUTF()));
-    }
-
-    private <K, V> void write(Map<K, V> map, DataOutputStream dos, BiCustomConsumer<K, V> action) throws IOException {
-        dos.writeInt(map.size());
-        for (Map.Entry<K, V> entry : map.entrySet())
-            action.accept(entry.getKey(), entry.getValue());
-    }
-
-    private <T> void write(T section, CustomConsumer<T> action) throws IOException {
-        action.accept(section);
-    }
-
-    private <T> void write(List<T> list, DataOutputStream dos, CustomConsumer<T> action) throws IOException {
-        dos.writeInt(list.size());
-        for (T t : list) {
-            action.accept(t);
+    private <T> void write(Collection<T> collection, DataOutputStream dos, CustomConsumer<T> action) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            action.accept(item);
         }
     }
 
@@ -148,19 +130,32 @@ public class DataStreamStrategy implements Serialization {
         return false;
     }
 
-    private <T> void read(T map, DataInputStream dis, CustomConsumer<T> action) throws IOException {
+    private void writeLocalDate(LocalDate date, DataOutputStream dos) throws IOException {
+        dos.writeInt(date.getYear());
+        dos.writeInt(date.getMonth().getValue());
+    }
+
+    private void read(DataInputStream dis, CustomRunnable action) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
-            action.accept(map);
+            action.run();
         }
     }
 
-    private <T> List<T> readList(DataInputStream dis, CustomConsumer<List<T>> action) throws IOException {
+    private List<String> readItems(DataInputStream dis) throws IOException {
+        return readList(dis, dis::readUTF);
+    }
+
+    private <T> List<T> readList(DataInputStream dis, CustomSupplier<T> action) throws IOException {
         int size = dis.readInt();
         List<T> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            action.accept(list);
+            list.add(action.get());
         }
         return list;
+    }
+
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
+        return LocalDate.of(dis.readInt(), dis.readInt(), 1);
     }
 }
