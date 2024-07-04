4d531174609d6926340c4e7e54161e68c0be7858
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-07-04 20:34:08 +0800

    create сonfig\
    - create init_db.sql
    - create resumes.properties

---
+++ config/init_db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE resume (
+    PRIMARY KEY (uuid),
+    uuid      CHAR(36)  NOT NULL,
+    full_name TEXT      NOT NULL
+);
+
+CREATE TABLE contact (
+    PRIMARY KEY (id),
+    id          SERIAL,
+    type        TEXT      NOT NULL,
+    value       TEXT      NOT NULL,
+    resume_uuid CHAR(36)  NOT NULL
+                REFERENCES resume (uuid)
+                ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX contact_uuid_type_index
+    ON contact (resume_uuid, type);
