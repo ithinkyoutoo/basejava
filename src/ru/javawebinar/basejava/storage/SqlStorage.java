5aae75c9cab36f6a636862cbce3d064096f0ef97
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-07-06 18:11:59 +0800

    update & fix SqlStorage
    - do private sqlHelper.
    - move "throw new NotExistStorageException() & ExistStorageException()" to SqlHelper.
    - fix update(), save(), get(), delete(), getAllSorted().
    - update size().

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -1,7 +1,5 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
-import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.sql.SqlHelper;
 
@@ -13,7 +11,7 @@ import java.util.List;
 
 public class SqlStorage implements Storage {
 
-    public final SqlHelper sqlHelper;
+    private final SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
@@ -27,55 +25,48 @@ public class SqlStorage implements Storage {
     @Override
     public void update(Resume r) {
         String sql = "UPDATE resume r SET full_name = ? WHERE r.uuid = ?";
-        sqlHelper.execute(sql, (ps) -> {
+        String uuid = r.getUuid();
+        sqlHelper.execute(uuid, sql, (ps) -> {
             ps.setString(1, r.getFullName());
-            ps.setString(2, r.getUuid());
-            if (ps.executeUpdate() == 0) {
-                throw new NotExistStorageException(r.getUuid());
-            }
+            ps.setString(2, uuid);
+            return ps.executeUpdate() != 0;
         });
     }
 
     @Override
     public void save(Resume r) {
-        String sql = "INSERT INTO resume (uuid, full_name) VALUES (?,?) ON CONFLICT (uuid) DO NOTHING";
-        sqlHelper.execute(sql, (ps) -> {
-            ps.setString(1, r.getUuid());
+        String sql = "INSERT INTO resume (uuid, full_name) VALUES (?,?)";
+        String uuid = r.getUuid();
+        sqlHelper.execute(uuid, sql, (ps) -> {
+            ps.setString(1, uuid);
             ps.setString(2, r.getFullName());
-            if (ps.executeUpdate() == 0) {
-                throw new ExistStorageException(r.getUuid());
-            }
+            return ps.executeUpdate();
         });
     }
 
     @Override
     public Resume get(String uuid) {
         String sql = "SELECT * FROM resume r WHERE r.uuid = ?";
-        return sqlHelper.executeAndGet(sql, (ps) -> {
+        return sqlHelper.execute(uuid, sql, (ps) -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            return new Resume(uuid, rs.getString("full_name"));
+            return rs.next() ? new Resume(uuid, rs.getString("full_name")) : false;
         });
     }
 
     @Override
     public void delete(String uuid) {
         String sql = "DELETE FROM resume r WHERE r.uuid = ?";
-        sqlHelper.execute(sql, (ps) -> {
+        sqlHelper.execute(uuid, sql, (ps) -> {
             ps.setString(1, uuid);
-            if (ps.executeUpdate() == 0) {
-                throw new NotExistStorageException(uuid);
-            }
+            return ps.executeUpdate() != 0;
         });
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        String sql = "SELECT uuid FROM resume";
-        return sqlHelper.executeAndGet(sql, (ps) -> {
+        String sql = "SELECT full_name, uuid FROM resume r ORDER BY r.full_name, r.uuid";
+        return sqlHelper.execute(sql, (ps) -> {
             List<Resume> resumes = new ArrayList<>();
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
@@ -87,6 +78,11 @@ public class SqlStorage implements Storage {
 
     @Override
     public int size() {
-        return getAllSorted().size();
+        String sql = "SELECT count(uuid) FROM resume";
+        return sqlHelper.execute(sql, (ps) -> {
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        });
     }
 }
