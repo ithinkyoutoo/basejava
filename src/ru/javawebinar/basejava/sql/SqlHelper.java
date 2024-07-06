7ad1e3e2dcab4da3b375653e2f45268be7343f3c
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-07-06 18:10:42 +0800

    update SqlHelper
    - update void execute(String sql, SqlRunnable action) to
      <T> T execute(String uuid, String sql, SqlSupplier action):
        - move "throw new NotExistStorageException()" from SqlStorage.
        - remove Connection & PreparedStatement.
    
    - update & rename executeAndGet() to execute():
      - move "throw new ExistStorageException()" from SqlStorage.

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.basejava.sql;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 
 import java.sql.Connection;
@@ -8,20 +10,24 @@ import java.sql.SQLException;
 
 public record SqlHelper(ConnectionFactory connectionFactory) {
 
-    public void execute(String sql, SqlRunnable action) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            action.run(ps);
-        } catch (SQLException e) {
-            throw new StorageException(e);
+    private static final int SQLSTATE_UNIQUE_VIOLATION = 23505;
+
+    public <T> T execute(String uuid, String sql, SqlSupplier action) {
+        T result = execute(sql, action);
+        if (result.equals(false)) {
+            throw new NotExistStorageException(uuid);
         }
+        return result;
     }
 
-    public <T> T executeAndGet(String sql, SqlSupplier action) {
+    public <T> T execute(String sql, SqlSupplier action) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             return (T) action.get(ps);
         } catch (SQLException e) {
+            if (Integer.parseInt(e.getSQLState()) == SQLSTATE_UNIQUE_VIOLATION) {
+                throw new ExistStorageException(e);
+            }
             throw new StorageException(e);
         }
     }
