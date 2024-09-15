a74ad6747826efe87d3c2d16a4ba6de0b5795f19
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-09-15 21:24:28 +0800

    fix init_db
    - rename:
      - contact: type, value to c_type, c_value.
      - section: type, value to s_type, s_value.

--- config/init_db.sql
+++ config/init_db.sql
@@ -7,25 +7,25 @@ CREATE TABLE resume (
 CREATE TABLE contact (
     PRIMARY KEY (id),
     id          SERIAL,
-    type        TEXT      NOT NULL,
-    value       TEXT      NOT NULL,
+    c_type      TEXT      NOT NULL,
+    c_value     TEXT      NOT NULL,
     resume_uuid CHAR(36)  NOT NULL
                 REFERENCES resume (uuid)
                 ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX contact_uuid_type_index
-    ON contact (resume_uuid, type);
+    ON contact (resume_uuid, c_type);
 
 CREATE TABLE section (
     PRIMARY KEY (id),
     id          SERIAL,
-    type        TEXT      NOT NULL,
-    value       TEXT      NOT NULL,
+    s_type      TEXT      NOT NULL,
+    s_value     TEXT      NOT NULL,
     resume_uuid CHAR(36)  NOT NULL
                 REFERENCES resume (uuid)
                 ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX section_uuid_type_index
-    ON section (resume_uuid, type);
+    ON section (resume_uuid, s_type);
