a158fefef6e49e1b117bbea66bc1dbd776e433fd
ithinkyoutoo <ithinkyoutoo@gmail.com>
2024-09-23 18:26:53 +0800

    fix init_db: columns name

--- config/init_db.sql
+++ config/init_db.sql
@@ -7,25 +7,25 @@ CREATE TABLE resume (
 CREATE TABLE contact (
     PRIMARY KEY (id),
     id          SERIAL,
-    c_type      TEXT      NOT NULL,
-    c_value     TEXT      NOT NULL,
+    type        TEXT      NOT NULL,
+    value       TEXT      NOT NULL,
     resume_uuid CHAR(36)  NOT NULL
                 REFERENCES resume (uuid)
                 ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX contact_uuid_type_index
-    ON contact (resume_uuid, c_type);
+    ON contact (resume_uuid, type);
 
 CREATE TABLE section (
     PRIMARY KEY (id),
     id          SERIAL,
-    s_type      TEXT      NOT NULL,
-    s_value     TEXT      NOT NULL,
+    type        TEXT      NOT NULL,
+    value       TEXT      NOT NULL,
     resume_uuid CHAR(36)  NOT NULL
                 REFERENCES resume (uuid)
                 ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX section_uuid_type_index
-    ON section (resume_uuid, s_type);
+    ON section (resume_uuid, type);
